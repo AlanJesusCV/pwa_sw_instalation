1e684cb31ca42e67d470d87e6861a0c16694d103
AlanJesusCV <alanjesuscv@gmail.com>
2021-09-20 21:25:18 -0500

    Agregar manifest y icons

--- js/app.js
+++ js/app.js
@@ -1,10 +1,15 @@
 console.log('hola mundo app.js')
+let url =window.location.href;
+let swDirect = '/pwa_sw_instalation/sw.js';
 
 // Preguntamos si navegador y/o el sitio dispone de sw
 if(navigator.serviceWorker){
     console.log('si, esta disponible');
     // Registrar el service worker
-    navigator.serviceWorker.register('/sw.js')
+    if (url.includes('localhost')){
+        swDirect='/sw.js'
+    }
+    navigator.serviceWorker.register(swDirect);
 }else{
     console.log('no, esta disponible')
 }
